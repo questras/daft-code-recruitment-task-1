014fc2e08b74854717134dd026acc31a3946335b
questras <michu123qwe@gmail.com>
2021-05-21 15:20:49 +0200

    Update allowed hosts.

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.environ.get('DEBUG'))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['short-messages.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
